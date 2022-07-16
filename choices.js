3ac76cb0f1d05c8285665aafbf9562d5ae320b19
Cyril Jephery <cyriljeph@gmail.com>
2022-07-16 08:51:33 +0100

    first commit

--- choices.js
+++ choices.js
@@ -18,7 +18,38 @@ function choiceStory(select) {
             }
             else {
                 selection = 3;
+                document.getElementById("choiceImg").src = "./assets/images/choice_3.png";
+            }
+            break;
+        case 2:
+            if (select == 1) {
+                selection = 4;
+                document.getElementById("choiceImg").src = "./assets/images/choice_4.png";
+            }
+            else {
+                selection = 5;
+                document.getElementById("choiceImg").src = "./assets/images/choice_5.png";
+            }
+            break;
+        case 3:
+            if (select == 1) {
+                selection = 6;
+                document.getElementById("choiceImg").src = "./assets/images/choice_6.png";
+            }
+            else {
+                selection = 7
+                document.getElementById("choiceImg").src = "./assets/images/choice_7.png";
 
             }
+                break;
+            case 4:
+                if (select == 1) {
+                    selection = 8;
+                    document.getElementById("choiceImg").src = "./assets/images/choice_8.png";
+                }
+                else {
+                    selection = 7
+                }
+                break;
     }
 }
